a77eca0c9e7518bc3721c954aae96b3c1c349fec
Andreas Meier <andreas.meier@stud.unibas.ch>
2022-01-29 13:09:17 +0100

    Plot delta of temperatures

--- report.py
+++ report.py
@@ -1,11 +1,11 @@
 import datetime
 
+import matplotlib.pyplot as plt
+import pandas
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-import models
-import pandas
-import matplotlib.pyplot as plt
 
+import models
 from main import get_db_uri
 
 ALIGNMENT_CUTOFF = datetime.timedelta(minutes=15)
@@ -19,8 +19,8 @@ def get_max_by_month():
     pass
 
 
-def find_closest_datapoint_in_time(
-    a: pandas.Series, b: pandas.DataFrame
+def pair_closest_datapoint_in_time(
+    a: pandas.Series, a_name: str, b: pandas.DataFrame, colname: str
 ) -> pandas.Series:
     timestamp = a["timestamp"]
     start = timestamp - ALIGNMENT_CUTOFF
@@ -30,13 +30,23 @@ def find_closest_datapoint_in_time(
         return None
     else:
         asdf = candidates.loc[[b["timestamp"].sub(timestamp).abs().idxmin()]]
-        return
+        return pandas.Series(
+            {
+                "timestamp": timestamp,
+                f"{colname}_{a_name}": a[colname],
+                f"{colname}_{b.name}": asdf[colname],
+                f"delta_{colname}": asdf[colname].values[0] - a[colname],
+            }
+        )
 
 
 def match_values(
     a: pandas.DataFrame, b: pandas.DataFrame, colname: str
 ) -> pandas.DataFrame:
-    return [find_closest_datapoint_in_time(x[1], b) for x in a.iterrows()]
+    temp = [
+        pair_closest_datapoint_in_time(x[1], a.name, b, colname) for x in a.iterrows()
+    ]
+    return pandas.DataFrame.from_records([x for x in temp if x is not None])
 
 
 if __name__ == "__main__":
@@ -54,10 +64,17 @@ if __name__ == "__main__":
         )
         balcony = pandas.read_csv("BALCONY.CSV")
         balcony["timestamp"] = pandas.to_datetime(balcony["timestamp"])
+        balcony.name = "balcony"
         datty_frame["timestamp"] = pandas.to_datetime(datty_frame["timestamp"])
+        datty_frame.name = "meteo"
         ax = datty_frame.plot(x="timestamp", y="temperature")
         balcony.plot(x="timestamp", y="temperature", ax=ax)
+
         # balcony.Timestamp.groupby(pandas.Grouper(freq='M'))
         plt.show()
-        match_values(datty_frame, balcony, "temperature")
+
+        matched_temps = match_values(datty_frame, balcony, "temperature")
+        matched_temps.plot(x="timestamp", y="delta_temperature")
+        plt.show()
+
         print("asdf")
