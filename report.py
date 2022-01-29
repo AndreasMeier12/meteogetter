d0b4399b7684e605be63afde5b3557ddf09fcd95
Andreas Meier <andreas.meier@stud.unibas.ch>
2022-01-29 12:39:08 +0100

    Draft matching of dataframes in time

--- report.py
+++ report.py
@@ -1,3 +1,5 @@
+import datetime
+
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import models
@@ -5,6 +7,36 @@ import pandas
 import matplotlib.pyplot as plt
 
 from main import get_db_uri
+
+ALIGNMENT_CUTOFF = datetime.timedelta(minutes=15)
+
+
+def get_min_by_month():
+    pass
+
+
+def get_max_by_month():
+    pass
+
+
+def find_closest_datapoint_in_time(
+    a: pandas.Series, b: pandas.DataFrame
+) -> pandas.Series:
+    timestamp = a["timestamp"]
+    start = timestamp - ALIGNMENT_CUTOFF
+    end = timestamp + ALIGNMENT_CUTOFF
+    candidates = b.query("timestamp > @start and timestamp < @end")
+    if len(candidates) == 0:
+        return None
+    else:
+        asdf = candidates.loc[[b["timestamp"].sub(timestamp).abs().idxmin()]]
+        return
+
+
+def match_values(
+    a: pandas.DataFrame, b: pandas.DataFrame, colname: str
+) -> pandas.DataFrame:
+    return [find_closest_datapoint_in_time(x[1], b) for x in a.iterrows()]
 
 
 if __name__ == "__main__":
@@ -22,8 +54,10 @@ if __name__ == "__main__":
         )
         balcony = pandas.read_csv("BALCONY.CSV")
         balcony["timestamp"] = pandas.to_datetime(balcony["timestamp"])
+        datty_frame["timestamp"] = pandas.to_datetime(datty_frame["timestamp"])
         ax = datty_frame.plot(x="timestamp", y="temperature")
         balcony.plot(x="timestamp", y="temperature", ax=ax)
         # balcony.Timestamp.groupby(pandas.Grouper(freq='M'))
         plt.show()
+        match_values(datty_frame, balcony, "temperature")
         print("asdf")
