21c5b8d61e1117b9fd006a529788bcd3826aef04
Andreas Meier <andreas.meier@stud.unibas.ch>
2022-08-14 19:06:27 +0200

    Use only main station id when checking for missing data

--- monitor.py
+++ monitor.py
@@ -1,6 +1,8 @@
 # Import smtplib for the actual sending function
 import smtplib
 from datetime import datetime, timedelta
+from typing import Dict
+
 from origamibot import OrigamiBot as Bot
 
 # Import the email modules we'll need
@@ -14,10 +16,9 @@ import models
 from main import get_db_uri
 
 
-def send_notification(content: str, session: Session = None):
+def send_notification(content: str, config: Dict, session: Session = None):
     message = EmailMessage()
     message.set_content(content)
-    config = yaml.safe_load(open("conf/monitor.yaml"))
 
     bot = Bot(config["token"])
     bot.send_message(config["chat_id"], content)
@@ -30,7 +31,7 @@ def send_notification(content: str, session: Session = None):
     return
 
 
-def measuremnts_too_old(session: Session):
+def measuremnts_too_old(session: Session, station_id: int):
     dt = datetime.now()
 
     cutoff = dt - timedelta(hours=2)
@@ -38,6 +39,7 @@ def measuremnts_too_old(session: Session):
     if (
         session.query(models.TemperatureMeasurement)
         .filter(models.TemperatureMeasurement.timestamp > cutoff)
+        .filter(models.TemperatureMeasurement.station_id == station_id)
         .count()
         == 0
     ):
@@ -45,6 +47,7 @@ def measuremnts_too_old(session: Session):
     if (
         session.query(models.HumidityMeasurement)
         .filter(models.HumidityMeasurement.timestamp > cutoff)
+        .filter(models.HumidityMeasurement.station_id == station_id)
         .count()
         == 0
     ):
@@ -62,12 +65,13 @@ def monitor():
     engine = sqlalchemy.create_engine(get_db_uri())
     Session = sessionmaker(bind=engine)
     session = Session()
-    if measuremnts_too_old(session) and msg_due(session):
-        send_notification("There seems to be data missing")
+    config = yaml.safe_load(open("conf/monitor.yaml"))
+    try:
+        if measuremnts_too_old(session, config["station_id"]) and msg_due(session):
+            send_notification("There seems to be data missing", config, session=session)
+    except Exception as e:
+        send_notification(f"Could not corry or something {str(e)}", config)
 
 
 if __name__ == "__main__":
-    try:
-        monitor()
-    except Exception as e:
-        send_notification(f"Could not corry or something {str(e)}")
+    monitor()
